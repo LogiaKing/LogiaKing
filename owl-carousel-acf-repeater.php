4477e35fe02a829adb200882c7803c9d8cc93973
logiaking <38210138+logiaking@users.noreply.github.com>
2021-05-23 23:08:44 +0100

    Update owl-carousel-acf-repeater.php

--- owl-carousel-acf-repeater.php
+++ owl-carousel-acf-repeater.php
@@ -16,31 +16,3 @@
    </div>
 </div>
 <!----> 
-
-
-//Init file
-<script>
-$('.owl-carousel-global').owlCarousel({
-  loop: true,
-  margin: 0,
-  nav: true,
-  autoWidth: false,
-  autoplay: true,
-  autoplayTimeout: 5000,
-  autoplayHoverPause: true,
-  stagePadding: 0,
-  singleItem: true,
-  navText: ['<span class="fas fa-chevron-circle-left fa-2x"></span>', '<span class="fas fa-chevron-circle-right fa-2x"></span>'],
-  responsive: {
-    0: {
-      items: 1
-    },
-    600: {
-      items: 1
-    },
-    1000: {
-      items: 1
-    }
-  }
-});
-<script>
